2b24e735ae0df649da1811bf2553276efeead01e
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2023-05-02 16:56:57 +0300

    Create prototypes

---
+++ dogs.js
@@ -0,0 +1,27 @@
+function Dog(name, breed, weight) { // Dog constructor
+    this.name = name;
+    this.breed = breed;
+    this.weight = weight;
+}
+
+Dog.prototype.species = 'Canine'; // Add 1 property and 3 methods to prototype
+
+Dog.prototype.bark = function() {
+    if (this.weght > 25) {
+        console.log(this.name + ' says Woof!');
+    } else {
+        console.log(this.name + ' says Yip!');
+    }
+};
+
+Dog.prototype.run = function() {
+    console.log('Run!');
+};
+
+Dog.prototype.wag = function() {
+    console.log('Wag!');
+};
+
+
+
+
