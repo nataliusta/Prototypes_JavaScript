c7713ce197a5037a6c77be884535830b8936b692
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2023-05-04 14:25:38 +0300

    Add tests

--- show_dogs.js
+++ show_dogs.js
@@ -29,7 +29,6 @@ function ShowDog(name, breed, weight, handler) { // constructor ShowDog
 
 ShowDog.prototype = new Dog(); // prototype ShowDog
 ShowDog.prototype.constructor = ShowDog; // specify constructor for ShowDog objects
-
 ShowDog.prototype.league = 'Webville'; // prototype's property
 
 ShowDog.prototype.stack = function() { // prototype's method
@@ -48,9 +47,17 @@ ShowDog.prototype.groom = function() { // ...
     console.log('Groom');
 };
 
+let fido = new Dog('Fido', 'Mixed', 38); // create new objects using operator New + constructor 
+let fluffy = new Dog('Fluffy', 'Poodle', 30);
+let spot = new Dog('Spot', 'Chihuahua', 10);
 let scotty = new ShowDog('Scotty', 'Dcottish Terrier', 15, 'Cookie'); // new object
+let beatrice = new ShowDog('Beatrice', 'Pomeranian', 5, 'Hamilton');
 
-/*scotty.stack();
+/*
+fido.bark();
+fluffy.bark();
+scotty.stack();
 scotty.bark();
+scotty.gait(')
 console.log(scotty.league);
 console.log(scotty.species);*/ // test
